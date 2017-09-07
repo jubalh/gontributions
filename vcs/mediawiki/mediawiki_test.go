59285ec28d8aba805a4a7606c138c4d000f1a8cf
Michael Vetter <jubalh@openmailbox.org>
2017-09-07 17:14:36 +0200

    Increase Wiki test coverage

--- vcs/mediawiki/mediawiki_test.go
+++ vcs/mediawiki/mediawiki_test.go
@@ -6,8 +6,12 @@ import "testing"
 // And LXQt has it's own Wiki now so I won't edit this anymore.
 // This makes it save as a testing environment since the edits won't change.
 const wikiURL = "http://wiki.lxde.org/en"
+const invalidURL = "hobbit://mordor.me"
+const noURL = "://.."
 const wikiUsername = "Jubalh"
 const expectedEdits = 8
+const expErrorInvalidURL = "Not a valid URL"
+const expErrorNoHTTPGet = "Not able to HTTP GET"
 
 func TestGetUserEdits(t *testing.T) {
 	t.Logf("Querying LXDE wiki for user 'Jubalh'")
@@ -18,5 +22,35 @@ func TestGetUserEdits(t *testing.T) {
 	}
 	if count != expectedEdits {
 		t.Errorf("GetUserEdits returned: %d, expected: %d", count, expectedEdits)
+	}
+}
+
+func TestGetUserEditsInvalidURL(t *testing.T) {
+	t.Logf("Querying invalid URL")
+	_, err := GetUserEdits(invalidURL, wikiUsername)
+
+	if err == nil {
+		t.Errorf("Should have failed because used invalid URL: %s", invalidURL)
+		t.FailNow()
+	}
+
+	if err.Error() != expErrorNoHTTPGet {
+		t.Errorf("Should have failed because with '%s' but got: '%s'", expErrorNoHTTPGet, err.Error())
+		t.FailNow()
+	}
+}
+
+func TestGetUserEditsNoHTTPGet(t *testing.T) {
+	t.Logf("Parse a non-URL")
+	_, err := GetUserEdits(noURL, wikiUsername)
+
+	if err == nil {
+		t.Errorf("Should have failed because used URL: %s", invalidURL)
+		t.FailNow()
+	}
+
+	if err.Error() != expErrorInvalidURL {
+		t.Errorf("Should have failed because with '%s' but got: '%s'", expErrorInvalidURL, err.Error())
+		t.FailNow()
 	}
 }
